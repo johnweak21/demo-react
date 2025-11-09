6bafe68fe4770b046eb77628f8b65a25267e9e91
johnweak21 <hh10330065@gmail.com>
2025-11-09 08:01:59 +0800

    Update GuideBar.jsx

--- src/GuideBar.jsx
+++ src/GuideBar.jsx
@@ -1,11 +1,39 @@
-import "bootstrap/dist/css/bootstrap.css";
+import { useState } from "react";
+const contact = {
+  name: "吳先生",
+  phone: "+1234567890",
+  email: "sirwu@example.com",
+};
 function GuideBar() {
+  // 控制模態框顯示與否的狀態
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  // 顯示模態框
+  const openModal = () => setIsModalOpen(true);
+  // 關閉模態框
+  const closeModal = () => setIsModalOpen(false);
   return (
-    <div className="GuideBar">
-      <button className="Btn">首頁</button>
-      <button className="Btn">購物車</button>
-      <button className="Btn" >聯絡人</button>
-    </div>
+    <>
+      <button className="btn btn-secondary w-100" onClick={openModal}>
+        聯絡人
+      </button>
+             {isModalOpen && (
+          <div className="modal">
+            <div className="modal-content">
+              <h2>{contact.name}</h2>
+              <p>
+                <strong>電話:</strong> {contact.phone}
+              </p>
+              <p>
+                <strong>電子郵件:</strong> {contact.email}
+              </p>
+
+              {/* 關閉模態框的按鈕 */}
+              <button onClick={closeModal}>關閉</button>
+            </div>
+          </div>
+        )}
+    </>
   );
 }
 export default GuideBar;
